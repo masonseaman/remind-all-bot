6580b063d4e012cc923463c035a54012a274e3b0
Mason <masonseaman@gmail.com>
2017-11-07 23:12:06 -0500

    moved to telebot

--- remind.py
+++ remind.py
@@ -1,37 +1,9 @@
+import telebot
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-import logging
+bot = telebot.TeleBot("400097540:AAF6iHaEHszGa7imNitGqdewGRdsWNOtJoM")
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+	bot.reply_to(message, "Hi I'm RemindAllBot, I'll send you reminders! Type /remind to get started!")
 
-#help command
-def help(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text="Welcome to reminder bot, I can send you reminders. Try /remind")
-		
-help_handler = CommandHandler('help', help)
-
-#remind command
-def remind(bot, update):
-	
-
-def main():
-	updater = Updater(token="400097540:AAF6iHaEHszGa7imNitGqdewGRdsWNOtJoM")
-	dispatcher = updater.dispatcher
-
-
-	#enable commands
-	dispatcher.add_handler(help_handler)
-	dispatcher.add_handler(CallbackQueryHandler(button))
-	dispatcher.add_handler(CommandHandler('remind',remind))
-
-	#enable messages
-
-	updater.start_polling()
-
-	updater.idle()
-
-if __name__ == '__main__':
-	main()
-
+bot.polling()
