0fdba1ec1a5f8db0d608fec74d94e77058de0d59
Mason <masonseaman@gmail.com>
2017-11-09 00:47:56 -0500

    initial try at steps

--- remind.py
+++ remind.py
@@ -4,6 +4,9 @@ from telebot import types
 
 bot = telebot.TeleBot("400097540:AAF6iHaEHszGa7imNitGqdewGRdsWNOtJoM")
 
+mins = ""
+reminder = ""
+
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
 	bot.reply_to(message, "Hi I'm RemindAllBot,x I'll send you reminders! Type /remind to get started!")
@@ -11,8 +14,23 @@ def send_welcome(message):
 @bot.message_handler(commands=['remind'])
 def start_remind_queries(message):
 	markup = types.ForceReply(selective=False)
-	bot.reply_to(message,"In how many minutes would you like to be reminded?",reply_markup=markup)
-	mins = message.text
-	bot.send_message(message.chat.id, mins)
-	
+	msg = bot.reply_to(message,"In how many minutes would you like to be reminded?",reply_markup=markup)
+	bot.register_next_step_handler(msg, process_ mins_step)
+
+def process_mins_step(message):
+	try:
+        chat_id = message.chat.id
+        mins = message.text
+        markup = types.ForceReply(selective=False)
+        msg = bot.reply_to(message,"What would you like the reminder to say?", reply_markup = markup)
+        bot.register_next_step_handler(msg, process_reminder_step)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+def process_reminder_step(message):
+	try:
+		chat_id = message.chat.id
+		reminder = message.text
+		bot.send_message(chat_id, "Great! I'll remind you to " + reminder + " in " + mins + " minutes!")
+
 bot.polling()
