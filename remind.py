4e294bc4b13386eb9c38fc9bbc61bd5275893c5a
Mason <masonseaman@gmail.com>
2017-11-08 23:43:50 -0500

    /reply stuff'

--- remind.py
+++ remind.py
@@ -4,6 +4,12 @@ bot = telebot.TeleBot("400097540:AAF6iHaEHszGa7imNitGqdewGRdsWNOtJoM")
 
 @bot.message_handler(commands=['start','help'])
 def send_welcome(message):
-	bot.reply_to(message, "Hi I'm RemindAllBot, I'll send you reminders! Type /remind to get started!")
+	bot.reply_to(message, "Hi I'm RemindAllBot,x I'll send you reminders! Type /remind to get started!")
 
+@bot.message_handler(commands=['remind'])
+def start_remind_queries(message):
+	markup = types.ForceReply(selective=False)
+	bot.reply_to(message,"In how many minutes would you like to be reminded?",reply_markup=markup)
+	mins = message.text
+	
 bot.polling()
