b0981d7d2f929a2e72e37984c94f031122f0b20d
Mason <masonseaman@gmail.com>
2017-11-09 18:31:08 -0500

    store vars as local thread variables to support multiple reminders

--- remind.py
+++ remind.py
@@ -8,13 +8,7 @@ import threading
 
 bot = telebot.TeleBot("400097540:AAF6iHaEHszGa7imNitGqdewGRdsWNOtJoM")
 
-chat_id = ""
-
-mins = ""
-reminder = ""
-
-def time_loop(minutes):
-	global mins, reminder
+def time_loop(chat_id, minutes, reminder):
 	seconds = int(minutes) * 60
 	while(seconds > 0):
 		seconds = seconds - 1
@@ -30,28 +24,27 @@ def send_welcome(message):
 @bot.message_handler(commands=['remind'])
 def start_remind_queries(message):
 	markup = types.ForceReply(selective=False)
-	msg = bot.reply_to(message,"In how many minutes would you like to be reminded?",reply_markup=markup)
-	bot.register_next_step_handler(msg, process_mins_step)
-
-def process_mins_step(message):
-	try:
-		global mins
-		global chat_id
-		chat_id = message.chat.id
-		mins = message.text
-		markup = types.ForceReply(selective=False)
-		msg = bot.reply_to(message,"What would you like the reminder to say?", reply_markup = markup)
-		bot.register_next_step_handler(msg, process_reminder_step)
-	except Exception as e:
-		bot.reply_to(message, 'oooops')
+	msg = bot.reply_to(message,"What would you like the reminder to say",reply_markup=markup)
+	bot.register_next_step_handler(msg, process_reminder_step)
 
 def process_reminder_step(message):
 	try:
-		global reminder, mins
 		chat_id = message.chat.id
 		reminder = message.text
+		markup = types.ForceReply(selective=False)
+		msg = bot.reply_to(message,"In how many minutes would you like to be reminded?", reply_markup = markup)
+		bot.register_next_step_handler(msg + "\n" + reminder, process_reminder_step)
+	except Exception as e:
+		bot.reply_to(message, 'oooops')
+
+def process_mins_step(message):
+	try:
+		chat_id = message.chat.id
+		arr = message.split('\n', 1)
+		mins = arr[0]
+		reminder = arr[1]
 		bot.send_message(chat_id, "Great! I'll remind you to " + reminder + " in " + mins + " minutes!")
-		t1=threading.Thread(target=time_loop, args=(mins))
+		t1=threading.Thread(target=time_loop, args=(chat_id, mins, reminder))
 		t1.start()
 	except Exception as e:
 		bot.reply_to(message, 'oooops')
