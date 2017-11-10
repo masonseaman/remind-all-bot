4544ad99b2cfd17fcd232762af91bd612fcd89ea
Mason <masonseaman@gmail.com>
2017-11-09 19:16:47 -0500

    bug fix

--- remind.py
+++ remind.py
@@ -44,7 +44,7 @@ def process_reminder_step(message):
 def process_mins_step(message):
 	try:
 		print(message)
-		arr = message.text.split('\t', 1)
+		arr = message.split('\t', 1)
 		print(arr)
 		chat_id = int(arr[0])
 		reminder = arr[1]
