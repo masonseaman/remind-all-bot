9d0328201dc2107d99d498c40d859862ab39b805
Mason <masonseaman@gmail.com>
2017-11-09 19:18:36 -0500

    bug fix

--- remind.py
+++ remind.py
@@ -35,8 +35,8 @@ def process_reminder_step(message):
 		reminder = message.text
 		markup = types.ForceReply(selective=False)
 		msg = bot.reply_to(message,"In how many minutes would you like to be reminded?", reply_markup = markup)
-		val = chat_id + "\t" + reminder + "\t" + msg.text
-		bot.register_next_step_handler(val, process_mins_step)
+		#val = chat_id + "\t" + reminder + "\t" + msg.text
+		bot.register_next_step_handler(msg, reminder, process_mins_step)
 	except Exception as e:
 		print(e)
 		bot.reply_to(message, "i goofed, try again")
